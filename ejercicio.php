e8eaace8cbf0117cb6063f80d8e1d56106540f1a
Alvaro Stockle <alvaro.stockle@gmail.com>
2018-04-18 14:44:30 -0300

    Segunda version

--- ejercicio.php
+++ ejercicio.php
@@ -5,7 +5,12 @@
 </head>
 <body>
 	<fieldset>
-		
+		<legend>Cálculo del índice de masa corporal</legend>
+		<form action="calculo.php" method="GET">
+			<label for="altura">Altura: </label><input type="" id="altura" name="altura" /> metros<br/>
+			<label for="peso">Peso: </label><input type="" id="peso" name="peso" /> kilogramos<br/>
+			<input type="submit" value="Calcular">
+		</form>
 	</fieldset>
 </body>
 </html>
